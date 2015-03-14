d48e90bc257c37cf71538b72b66b6ec87fe45b8c
anovus <marwin.quito@gmail.com>
2015-03-14 23:49:44 +0000

    Adding files

---
+++ prog.py
@@ -0,0 +1,13 @@
+
+
+#Prompt user for file name. Save file name to var
+file_name= raw_input('Enter file name: ')
+#Read file to memory
+f=open(file_name,'r')
+#Store file data to var
+f_data=f.read()
+#breakdown S19 records into list
+h=f_data.split('\r\n')
+
+
+print file_name + ' has ' + str(len(h)) + ' records.'
