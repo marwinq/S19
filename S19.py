8ca44b1a6efa65a8fd3d4db16e89b4a208d2482d
anovus <marwin.quito@gmail.com>
2015-03-26 21:25:35 -0500

    Updated S19 file with comments

--- S19.py
+++ S19.py
@@ -1,75 +1,98 @@
 #S19 File Analysis
+#By Marwin Quito
+#This is a small module which can be used to analyze the structure of
+#an S19 file for personal study and reference.
 
-##functions##
+###############
+#	Functions
+###############
 
-#Count SX records in the file
-def s_count(record_list, record_num):
-    c=0
-    r=record_list
-    l=len(r) #length of the list
-    s= "S"+str(record_num) #S0, S1, S2, S3, S4,..
-    for i in range(0, l-1):
-        t= s in r[i]
-        if t==True:
-            c+=1
+#Returns a count for records of the S[N] type, where N is any number from 0 through 9.
+
+def s_count(record_list, N):
+    c=0				#count var initalized at 0
+    r=record_list 	#local var for the list of records
+    l=len(r) 		#length of the record list (the S19 file)
+    s= "S"+str(N)		#Depending on value of N, a string equal to S0, S1, .., S8, or S9
+    for i in range(0, l-1):	#iterates for each member of the list
+        t= s in r[i]		#Is S[N] found in the current row? True if yes, False if no.
+        if t==True:			
+            c+=1 			#increment by 1 for each record of S[N] found
     return c
 
-#List the SX counts, where X is 0 through 9
+#Returns a list of 10 elements, where the first element is equal to the number of S0
+#records in the S19 file, the 2nd element equal to the number of S1 records, and so on
+#until S9.
+
 def s_totals(record_list):
-    r=record_list
-    S=zerolist(10)
+    r=record_list	#local var for the list of records
+    S=zerolist(10)	#initialize empty list.
     for i in range(0,9):
-        S[i]=s_count(r,i)
+        S[i]=s_count(r,i)		#count how many of S[N] type records in the S19 file.
     return S
 
-#Print Totals
+#A printed breakdown of the type of S records found in the S19 file. Omits S type records
+#that are not found in the file.
+
 def print_totals(S):
     for i in range(0,9):
         if S[i]!=0:
             print(str(S[i])+'\tS'+str(i)+' records.')
     return
 
-def zerolist(n):
-    l=[0]*n
+#Initialize a list of zeroes of length N
+
+def zerolist(N):
+    l=[0]*N
     return l
 
-#extracts the bytecount byte from record
-def bytecount_byte(s):
-    r=s[2:4]
+#Extracts the bytecount byte from the selected record R. See diagram XX for illustration of
+#this byte.
+
+def bytecount_byte(R):
+    r=R[2:4]
     return r
-#converts bytecount from hex to decimal value
+
+#Converts the bytecount byte from hex to decimal value.
+
 def bytecount(s):
     b=bytecount_byte(s)
     r=int(b,16)
     return r
 
-#extracts the checksum from a record
-def checksum(s):
-    r=s[-2:]
+#Extracts the checksum byte from the selected record R.
+
+def checksum(R):
+    r=R[-2:]
     return r
 
-#extracts the data from a record
-def data_extract(r):
-    adc=r[4:] #address, data, checksum
-    if 'S0' in r or 'S1' in r:
-        s=r[8:-2]
-    if 'S2' in r:
-        s=r[10:-2]
-    if 'S3' in r or 'S7' in r:
-        s=r[12:-2]
+#Extracts the data fields from the selected record R
+
+def data_extract(R):
+    adc=R[4:] 			#address, data, checksum
+    if 'S0' in R or 'S1' in R:
+        s=R[8:-2]
+    if 'S2' in R:
+        s=R[10:-2]
+    if 'S3' in R or 'S7' in R:
+        s=R[12:-2]
     return s
 
-def addr_extract(r):
-    adc=r[4:] #address, data, checksum
-    if 'S0' in r or 'S1' in r:
-        s=r[4:8]
-    if 'S2' in r:
-        s=r[4:10]
-    if 'S3' in r or 'S7' in r:
-        s=r[4:12]
+#Extracts the address fields from the selecter record R.
+
+def addr_extract(R):
+    adc=R[4:] #address, data, checksum
+    if 'S0' in R or 'S1' in R:
+        s=R[4:8]
+    if 'S2' in R:
+        s=R[4:10]
+    if 'S3' in R or 'S7' in R:
+        s=R[4:12]
     return s
 
-#extracts address data of an entire list of records
+#Extracts the address fields for all records in the records list.
+#Returns a list of the address fields.
+
 def addr_extract_whole(record_list):
     r=record_list
     l=len(record_list)
@@ -78,6 +101,9 @@ def addr_extract_whole(record_list):
         S[i]=addr_extract(r[i])
     return S
 
+#Extracts the data fields for all records in the records list.
+#Returns a list of the data fields.
+
 def data_extract_whole(record_list):
     r=record_list
     l=len(record_list)
@@ -85,12 +111,18 @@ def data_extract_whole(record_list):
     for i in range(0,l-1):
         S[i]=data_extract(r[i])
     return S
+
+#Saves the extracted data fields list to file.
+
 def dump_data(g):
     q=data_extract_whole(g)
-    data=open('_data.txt','w')
+    data=open(file_name+'_data.txt','w')
     for x in q:
         data.write("%s\n" % x)
     return
+
+#Saves the extracted address fields list to file.
+
 def dump_addresses(g):
     q=addr_extract_whole(g)
     data=open(file_name+'_addresses.txt','w')
@@ -98,27 +130,30 @@ def dump_addresses(g):
         data.write("%s\n" % x)
     return
 
-####MAIN PROGRAM####
-#Prompt user for file name. Save file name to var
+#####################
+#	Main Program	
+#####################
+
+#Prompt user for file name. Save file name to var.
 file_name= raw_input('Enter file name: ')
-#Read file to memory
+#Read file to memory.
 f=open(file_name,'r')
-#Store file data to var
+#Store file data to var.
 f_data=f.read()
-#breakdown S19 records into list
+#Breakdown S19 records into list.
 g='\r\n'
 if g in f_data:
     h=f_data.split(g)
 else:
     h=f_data.split('\n')
 
-#Print total number of records
+#Print total number of records.
 print file_name + ' has ' + str(len(h)) + ' records.'
 
-#Make list of the count of different type of S records
+#Make list of the count of different type of S records.
 z=s_totals(h)
 print_totals(z)
+#Save Data fields to file
 d_h=data_extract_whole(h)
+#Save Address fields to file
 a_h=addr_extract_whole(h)
-
-
